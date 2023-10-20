cb3dc57889de2fcadf41be935fc02e5edd2a421b
ema <smolic.ema.12@gmail.com>
2023-10-20 07:41:32 +0200

    add db

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var cors = require('cors')
 var app = express()
  
 app.use(cors())
+
+import { Client } from 'pg'
+const client = new Client('postgres://web_radionica_db_user:2kePjBU4mJCgbFemSTkY2Str74AfOqEo@dpg-ckp018oujous73c3qbg0-a/web_radionica_db')
+await client.connect()
+ 
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) =>  {
+  const data = await client.query('SELECT * from person')
+  res.send(data)
 })
 
 app.listen(port, () => {
